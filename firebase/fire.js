f9e0955e90d92e7a55c74f5bc78a05e281c819e7
Kennedy Njeri <mistakenz123@gmail.com>
2019-08-25 13:50:26 +0300

    print the data on the template

--- firebase/fire.js
+++ firebase/fire.js
@@ -1,5 +1,28 @@
+const list = document.querySelector("ul")
+
+
+const addRecipe = (recipe) => {
+    let html = `
+    <li>
+    <div>${recipe.title}</div>
+    </li>
+    
+    `
+    //console.log(html)
+
+    list.innerHTML += html
+}
+
+
+
+
+
 db.collection('recepies').get().then((snapshot) => {
-    console.log(snapshot.docs[0].data())
+    //console.log(snapshot.docs[0].data())
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data())
+        addRecipe(doc.data())
+    })
 }).catch(err =>{
     console.log(err)
 })
